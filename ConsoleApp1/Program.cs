869fa7fe4df2c42153a5cbbab618e6e4df686b53
VikaSeredenina <119499762+VikaSeredenina@users.noreply.github.com>
2023-12-18 20:50:13 +0300

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите число a: ");
+			int a = int.Parse(Console.ReadLine());
+			Console.WriteLine("Введите число b: ");
+			int b = int.Parse(Console.ReadLine());
+
+			int c = a + b;
+			Console.WriteLine("Сумма чисел равна: " + c);
+
+		}
+	}
+}
